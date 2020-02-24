a59dd8c963ab0adbe51de1eba8546d4505b49159
Scott Key <scottdkey@gmail.com>
2020-02-24 16:25:41 -0700

    added CRUD for Physicians - stable

--- app/controllers/physicians_controller.rb
+++ app/controllers/physicians_controller.rb
@@ -1,13 +1,48 @@
+# frozen_string_literal: true
+
 class PhysiciansController < ApplicationController
+  before_action :set_physician, only: %i[show edit update destroy]
   def index
+    @physicians = Physician.all
   end
 
   def new
+    @physician = Physician.new
   end
 
-  def edit
+  def create
+    @physician = Physician.new(physician_params)
+    if @physician.save
+      redirect_to @physician
+    else
+      render :new
+    end
   end
 
-  def show
+  def edit; end
+
+  def update
+    if @physician.update(physician_params)
+      redirect_to physician_path(@physician)
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @physician.destroy
+    redirect_to physicians_path
+  end
+
+  private
+
+  def set_physician
+    @physician = Physician.find(params[:id])
+  end
+
+  def physician_params
+    params.require(:physician).permit(:first_name, :last_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'patients/index'
-  get 'patients/new'
-  get 'patients/edit'
-  get 'patients/show'
-  get 'appointments/index'
-  get 'appointments/new'
-  get 'appointments/edit'
-  get 'appointments/show'
-  get 'physicians/index'
-  get 'physicians/new'
-  get 'physicians/edit'
-  get 'physicians/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'physicians#index'
+  resources :patients
+  resources :physicians
 end
